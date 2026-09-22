9d8ec2980a8318a1f66ebe6e8c73bd4fe0a105e0
Jan-1103 <jananiparthasarathy19@gmail.com>
2026-09-22 19:54:03 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello from Jenkins CI/CD pipeline!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
